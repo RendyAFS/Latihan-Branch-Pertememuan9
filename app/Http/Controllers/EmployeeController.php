4210dec18ca0cf84d1e46abdb27745ec09318a99
RendyAFS <arendy724@gmail.com>
2023-06-20 08:36:02 +0700

    menghapus fitur delete

--- app/Http/Controllers/EmployeeController.php
+++ app/Http/Controllers/EmployeeController.php
@@ -54,9 +54,7 @@ class EmployeeController extends Controller
      */
     public function destroy(string $id)
     {
-         // ELOQUENT
-    Employee::find($id)->delete();
-    return redirect()->route('employees.index');
+
     }
 
 }
